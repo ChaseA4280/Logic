8c351e441af88eac85d7729e98a77bc347f58863
Arthur <adamc52102@gmail.com>
2023-04-26 15:37:20 -0500

    Logic1

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿// written by Adam Chase 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicHW
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+
+
+
+            // convenience store 
+            questions aQues = new questions();
+            /*int[] change = { 2, 100, 0, 0 };
+            Console.WriteLine(aQues.ChangeEnough(change, 14.11));
+            Console.ReadLine();*/
+
+
+
+
+            // second largest number
+            /* int[] nums = { 10, 40, 30, 20, 50 };
+             Console.WriteLine(aQues.SecondLargest(nums));
+             Console.ReadLine();*/
+
+
+
+
+            // remove odd nums 
+            /*int[] nums2 = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int[] evens = aQues.NoOdds(nums2);
+            Console.WriteLine("\n the even elements are \n");
+            for (int i = 0; i < nums2.Length; i++)
+            {
+                Console.WriteLine(evens);
+            }
+            Console.ReadLine();*/
+
+
+
+
+            // switcharoo
+            /*string aString = "cat,dog,and mouse.";
+          
+            Console.WriteLine(aQues.Switch(aString)); 
+            
+            Console.ReadKey();*/
+
+
+
+            // second occurance of zip 
+
+            string aString = "all zip files are zipped";
+
+            Console.WriteLine(aQues.FindZip(aString));
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ questions.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.ExceptionServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicHW
+{
+    public class questions
+    {
+        // convenience store
+        public bool ChangeEnough(int[] change,double amtDue)
+        {
+            
+            double quarters = change[0] * .25;
+            double dimes = change[1] * .1;
+            double nickles = change[2] * .05;
+            double pennies = change[3] * .01;
+            double amt = quarters + dimes + nickles + pennies;
+            if ( amt >= amtDue )
+            {
+                return true;
+            }
+            else 
+            { 
+                return false;
+            }
+            
+        }
+        // second largest number 
+        public int SecondLargest(int[] nums)
+        {
+            int length = nums.Length;
+            int secL = nums[1];
+            int large = nums[0];
+            for (int i = 0; i < length; i++)
+            {
+                if (nums[0] > nums[i++])
+                {
+                    nums[0] = large;
+                }
+                else if (nums[1] < nums[i++]) 
+                {
+                    nums[1] = secL;
+
+                }
+                
+            }
+            return secL;
+        }
+        // eliminate odd numbers 
+        public int[] NoOdds(int[] nums)
+        {
+            int even = 0;
+            int odd = 0;
+
+            foreach (var n in nums)
+            {
+                
+                if ((n % 2) == 0)
+                {
+                    even++;
+                }
+                else if ((n % 2) != 1)
+                {
+                    odd++;
+                }
+
+               
+            }
+            int[] evens = new int[even];
+            return evens;
+
+           
+        }
+        // switcaroo
+        public string Switch(string aString)
+        {
+            // aquire the length of given string
+            int length = aString.Length;
+            string newString = string.Empty;
+            
+            if (length < 2)
+            {
+                return aString + "";
+            }
+            else if (length > 2 & aString[0] == aString[length - 1])
+            {
+                return aString + "incompatible";
+
+            }
+            else
+            {
+                for (int i = 1; i < length; i++)
+                {
+                    
+                    string first = aString.Substring(0,1);
+                    string remainder = aString.Substring(1,length-2) ;
+                    string last = aString.Substring(length-1);
+                    newString = $"{last}{remainder}{first}";
+                   
+                    
+                   
+                }
+            }
+            return newString;
+           
+            
+        }
+        // find the second occurnce of "zip" in a string
+        public int FindZip(string aString)
+        {
+            int length = aString.Length;
+            
+            int first = aString.IndexOf("zip");
+            int second = aString.IndexOf("zip", first + 1);
+
+            return second;
+            
+        }
+    } 
+}
